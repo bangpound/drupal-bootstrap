a3b2f465bd248bffeebc186c933fafd14067eafb
Benjamin Doherty <bjd@bangpound.org>
2014-01-22 11:24:05 -0600

    improve prefix handling and allow scanned paths to be overridden in subclasses.

--- src/Bangpound/Drupal/Composer/ScriptHandler.php
+++ src/Bangpound/Drupal/Composer/ScriptHandler.php
@@ -8,42 +8,59 @@ use Symfony\Component\Finder\Finder;
 
 class ScriptHandler
 {
+    /**
+     * Paths in Drupal root to scan for classes.
+     *
+     * This should never include profiles and sites, because those are scanned to
+     * generate separate classmaps.
+     *
+     * @var array
+     */
+    protected static $root_paths = array(
+        'includes', 'misc', 'modules', 'scripts', 'themes',
+
+        // None of these files actually contain PHP classes, but the are
+        // scanned anyway.
+        'authorize.php', 'cron.php', 'index.php', 'install.php',
+        'update.php', 'xmlrpc.php',
+    );
+
+    /**
+     * Paths in subdirectories (profiles and sites) to scan for classes.
+     *
+     * @var array
+     */
+    protected static $subdir_paths = array(
+        'modules', 'themes', 'plugins',
+    );
+
     public static function dumpAutoload(Event $event)
     {
+        $cwd = getcwd();
         $io = $event->getIO();
 
         $options = self::getOptions($event);
-        $prefix = $options['drupal-root'];
+        if (!empty($options['drupal-root'])) {
+            chdir($options['drupal-root']);
+        }
 
         $generator = new ClassMapGenerator();
-        $dirs = array(
-            $prefix . 'includes', $prefix . 'misc', $prefix . 'modules', $prefix . 'scripts',
-            $prefix . 'themes', $prefix . 'authorize.php', $prefix . 'cron.php', $prefix . 'index.php',
-            $prefix . 'install.php', $prefix . 'update.php', $prefix . 'xmlrpc.php',
-        );
-        $dirs = array_filter($dirs, 'file_exists');
+        $dirs = array_filter(self::$root_paths, 'file_exists');
         $io->write('Dumping classmap for <info>DRUPAL_ROOT</info>');
-        $generator->dump($dirs, $prefix . 'classmap.php');
+        $generator->dump($dirs, 'classmap.php');
 
         $finder = Finder::create()
             ->directories()
             ->depth(0)
             ->followLinks()
-            ->in(array($prefix . 'profiles', $prefix . 'sites'))
+            ->in(array('profiles', 'sites'))
         ;
 
-        $cwd = getcwd();
         foreach ($finder as $file) {
             /** @var \Symfony\Component\Finder\SplFileInfo $file */
             chdir($file->getPathInfo() .'/'. $file->getRelativePathname());
             $io->write(sprintf('Dumping classmap for <info>%s</info>', $file->getPathInfo() .'/'. $file->getRelativePathname()));
-            $dirs = array();
-            if (file_exists('modules')) {
-                $dirs[] = 'modules';
-            }
-            if (file_exists('themes')) {
-                $dirs[] = 'themes';
-            }
+            $dirs = array_filter(self::$subdir_paths, 'file_exists');
             $generator->dump($dirs, 'classmap.php');
             chdir($cwd);
         }
