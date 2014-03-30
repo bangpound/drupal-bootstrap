bf0888691e9725454409e3b777f452e6939cb7de
Benjamin Doherty <bjd@bangpound.org>
2014-03-30 02:25:35 -0500

    separate bootstrap functions into static class.

--- Bootstrap.php
+++ Bootstrap.php
@@ -8,67 +8,6 @@ namespace Drupal\Core;
  */
 class Bootstrap extends \Pimple implements BootstrapInterface
 {
-    /**
-     * Instantiate the container.
-     *
-     * Objects and parameters can be passed as argument to the constructor.
-     *
-     * @param array $values The parameters or objects.
-     */
-    public function __construct(array $values = array())
-    {
-
-        parent::__construct($values);
-
-        // Bootstrap phases are represented by a consecutive series of
-        // integer constants. These phases should not be changed at all.
-        // Use event listeners to add new functionality between
-        // existing bootstrap phases.
-        $this['phases'] = array(
-            DRUPAL_BOOTSTRAP_CONFIGURATION,
-            DRUPAL_BOOTSTRAP_PAGE_CACHE,
-            DRUPAL_BOOTSTRAP_DATABASE,
-            DRUPAL_BOOTSTRAP_VARIABLES,
-            DRUPAL_BOOTSTRAP_SESSION,
-            DRUPAL_BOOTSTRAP_PAGE_HEADER,
-            DRUPAL_BOOTSTRAP_LANGUAGE,
-            DRUPAL_BOOTSTRAP_FULL,
-        );
-
-        $this[DRUPAL_BOOTSTRAP_CONFIGURATION] = $this->share(function () {
-            _drupal_bootstrap_configuration();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_PAGE_CACHE] = $this->share(function () {
-            _drupal_bootstrap_page_cache();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_DATABASE] = $this->share(function () {
-            _drupal_bootstrap_database();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_VARIABLES] = $this->share(function () {
-            _drupal_bootstrap_variables();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_SESSION] = $this->share(function () {
-            require_once DRUPAL_ROOT . '/' . variable_get('session_inc', 'includes/session.inc');
-            drupal_session_initialize();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_PAGE_HEADER] = $this->share(function () {
-            _drupal_bootstrap_page_header();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_LANGUAGE] = $this->share(function () {
-            drupal_language_initialize();
-        });
-
-        $this[DRUPAL_BOOTSTRAP_FULL] = $this->share(function () {
-            require_once DRUPAL_ROOT . '/includes/common.inc';
-            _drupal_bootstrap_full();
-        });
-    }
 
     /**
      * @param  null  $phase     Phase
@@ -120,7 +59,7 @@ class Bootstrap extends \Pimple implements BootstrapInterface
      */
     protected function getPhases()
     {
-        return $this['phases'];
+        return $this->keys();
     }
 
     /**
---
+++ BootstrapPhases.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\Core;
+
+class BootstrapPhases
+{
+    public static function get()
+    {
+        return array(
+            // DRUPAL_BOOTSTRAP_CONFIGURATION
+            0 =>
+                function () {
+                    _drupal_bootstrap_configuration();
+                },
+
+            // DRUPAL_BOOTSTRAP_PAGE_CACHE
+            1 =>
+                function () {
+                    _drupal_bootstrap_page_cache();
+                },
+
+            // DRUPAL_BOOTSTRAP_DATABASE
+            2 =>
+                function () {
+                    _drupal_bootstrap_database();
+                },
+
+            // DRUPAL_BOOTSTRAP_VARIABLES
+            3 =>
+                function () {
+                    _drupal_bootstrap_variables();
+                },
+
+            // DRUPAL_BOOTSTRAP_SESSION
+            4 =>
+                function () {
+                    require_once DRUPAL_ROOT . '/' . variable_get('session_inc', 'includes/session.inc');
+                    drupal_session_initialize();
+                },
+
+            // DRUPAL_BOOTSTRAP_PAGE_HEADER
+            5 =>
+                function () {
+                    _drupal_bootstrap_page_header();
+                },
+
+            // DRUPAL_BOOTSTRAP_LANGUAGE
+            6 =>
+                function () {
+                    drupal_language_initialize();
+                },
+
+            // DRUPAL_BOOTSTRAP_FULL
+            7 =>
+                function () {
+                    require_once DRUPAL_ROOT . '/includes/common.inc';
+                    _drupal_bootstrap_full();
+                },
+        );
+    }
+}
